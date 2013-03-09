8f8c4e7b6a8013b33defac2b4b0230bf2ebb3e30
Justin Molineaux <justin.molineaux@gmail.com>
2013-03-09 04:54:26 -0500

    Removing extra </table> and </li> tags.

--- lib/diff_to_html.rb
+++ lib/diff_to_html.rb
@@ -126,9 +126,6 @@ EOF
         end
         @last_op = op
       end
-
-      flush_changes(result, left_ln, right_ln)
-      result << "</table></li>"      
     else
       #"<div class='error'>#{header_old}</div>"
       result =%Q{<li style="background-color:#eee; padding:2px; border:1px solid #ddd; border-radius:6px;"><h3 style="margin:5px; font-size:14px; font-weight:normal;">#{file_name}</h3>#{header_old}</li>}
